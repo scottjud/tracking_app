41a91fcf804407282f9f640430277b27758cfcef
Scott Judson <scottjud@gmail.com>
2013-10-02 22:50:23 -0400

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrackingApp::Application.config.secret_key_base = '52822fa160df6d36d039e1d76bbaa9904ec2b6d3b58ce2ed7bb7d886c6f8225e379ef94ef2dd3e6cbba93cd09d081b0eae891ed9e6495634f13c2ed5ed1b0c0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrackingApp::Application.config.session_store :cookie_store, key: '_tracking_app_session'
